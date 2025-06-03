b759f156b29ea4278b983b1d4a64baeb29b3c5ac
Lawrence Dass <23227030+lawrence-dass@users.noreply.github.com>
2025-06-02 21:51:23 -0700

    dotenv configuration

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 
+import { PORT } from './config/env.js';
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Welcome to subs-track API')
 });
 
-app.listen(3000, () => {
-    console.log('Subs-track API is running on https://localhost:3000')
+app.listen(PORT, () => {
+    console.log(`Subs-track API is running on https://localhost:${PORT}`)
 })
 
 export default app;
